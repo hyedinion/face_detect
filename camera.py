a54afbc3ac20f7cf89d2395d87dcd13668db8f30
hyedinion <ja03129@naver.com>
2020-08-10 21:54:22 +0900

    Add Opencv python file (camera, body)
    camera.py 파일 수정

--- camera.py
+++ camera.py
@@ -8,22 +8,7 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
     ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
-	
-	image = cv2.imread(frame)
-	grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-	body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'./haarcascade_frontalface_default.xml')
-	body = body_cascade.detectMultiScale(grayImage, 1.01, 10)
-
-	for (x,y,w,h) in body:
-		cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
-
-	plt.figure(figsize=(12,12))
-	plt.imshow(image, cmap='gray')
-	plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-	plt.show()
-	
+    cv2.imshow("VideoFrame", frame)	
     if cv2.waitKey(1) > 0: break
 
 capture.release()
